0db032c8fda320022b5d87b37dc72408b537428d
PawalAtakosi <91660424+PawalAtakosi@users.noreply.github.com>
2023-01-29 14:01:52 +0700

    commit1

---
+++ app/src/main/java/com/example/remedialptb/ListGempaActivity.java
@@ -0,0 +1,14 @@
+package com.example.remedialptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListGempaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_gempa);
+    }
+}
